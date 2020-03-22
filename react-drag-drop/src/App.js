3baf1a6f85c991e9a764c21061a4a3918a89eb38
Thakur Mahendar Singh <thakurmahendar.singh@accoliteindia.com>
2020-03-22 17:53:52 +0530

    drag and drop feature with deleting file functionality

--- react-drag-drop/src/App.js
+++ react-drag-drop/src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DragAndDrop from './DragAndDrop';
 
 function App() {
+
+  const initialState = {
+    dropDepth: 0,
+    inDropZone: false,
+    fileList: []
+  };
+
+  const reducer = (state= initialState, action) => {
+    switch(action.type) {
+      case 'SET_DROP_DEPTH':
+        return {...state, dropDepth: action.dropDepth}
+      case 'SET_IN_DROP_ZONE':
+        return {...state, inDropZone: action.inDropZone}
+      case 'ADD_FILE_TO_LIST':
+        return {...state, fileList: state.fileList.concat(action.files)};
+      case 'DELETE_FILE':
+        return {...state, fileList: action.newFilelist};
+      default:
+        return state;
+    }
+  };
+
+  const [data, dispatch] = useReducer(reducer, initialState);
+
+  const deleteHandler = (e, name) => {
+    const existingFiles = data.fileList;
+    let newFilelist = existingFiles.filter(item => item.name !== name);
+    dispatch({ type: 'DELETE_FILE', newFilelist });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React drag and drop component example</h1>
+      <DragAndDrop  data={data} dispatch={dispatch} />
+      {
+        data.fileList.length > 0 ?
+        ( <ol className="dropped-files">
+        {data.fileList.map(f => {
+          return (
+            <div class="w3-large"><li style={{float: 'left'}} key={f.name}>{f.name}</li><i onClick={e => deleteHandler(e, f.name)} class="fas fa-trash"></i>
+            </div>
+          )
+        })}
+      </ol>)
+        : null
+      }
+     
     </div>
   );
 }
